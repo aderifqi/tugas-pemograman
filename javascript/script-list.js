291c84c756e4374ee6c8ab8382b12b8d0bdf1800
ade <aderifqi85@gmail.com>
2020-12-26 16:06:55 +0700

    add javascript for list.html

---
+++ javascript/script-list.js
@@ -0,0 +1,10 @@
+fetch("./data/data-list.json",{mode:'no-cors'})
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
+
+//$.getJSON('./data/data-list.json',function(data){
+      //console.log(data)
+//})
+
